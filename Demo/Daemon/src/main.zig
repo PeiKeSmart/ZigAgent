73849467bfc7bb7526c5be282fa592763d7e7879
猿人易 <qcjxberin@outlook.com>
2024-08-20 23:05:22 +0800

    feat: Add Linux service functionality and tests

---
+++ Demo/Daemon/src/main.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const posix = std.posix;
+
+const log = @import("zzig").DebugLog;
+
+pub fn main() !void {
+    try daemon();
+}
+
+fn daemon() !void {
+    // 创建子进程
+    const pid = try posix.fork(); // 使用 try 来捕获错误
+
+    if (pid == 0) {
+        // 这是子进程的代码
+        std.debug.print("This is the child process with PID: {}\n", .{pid});
+    } else {
+        // 这是父进程的代码
+        std.debug.print("This is the parent process with child PID: {}\n", .{pid});
+    }
+}
